c1e9bffe5e27c7dfa203afba9287416f82059a70
cars0106 <cars0106@naver.com>
2019-09-09 21:03:41 +0900

    Splash 수정
    Signed-off-by: cars0106 <cars0106@naver.com>

--- app/src/main/java/com/example/itsability/SplashActivity.java
+++ app/src/main/java/com/example/itsability/SplashActivity.java
@@ -4,8 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-//스플래쉬 구현은 https://yongtech.tistory.com/100 여기를 참고했습니다.
-//로딩시간으로 초기화면 시간 설정한게 아니라 임의로 4초로 설정했는데, 나중에 수정할 예정
+//스플래쉬 구현은 https://yongtech.tistory.com/100, https://lx5475.github.io/2017/07/15/android-splash/ 여기를 참고했습니다.
+//로딩시간은 1초로 임의로 설정했습니다.
 
 public class SplashActivity extends Activity {
     @Override
@@ -14,11 +14,11 @@ public class SplashActivity extends Activity {
 
         try {
             Thread.sleep(1000);
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
         }
         catch (InterruptedException e) {
             e.printStackTrace();
         }
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
     }
 }
