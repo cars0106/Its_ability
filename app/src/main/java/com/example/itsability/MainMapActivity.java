82695f3ae2c66c76353ca37b673aab9548e8237d
cars0106 <cars0106@naver.com>
2019-09-12 16:49:48 +0900

    T-map Marker 추가
    Signed-off-by: cars0106 <cars0106@naver.com>

--- app/src/main/java/com/example/itsability/MainMapActivity.java
+++ app/src/main/java/com/example/itsability/MainMapActivity.java
@@ -1,10 +1,23 @@
 package com.example.itsability;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.skt.Tmap.TMapMarkerItem;
+import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 
 public class MainMapActivity extends AppCompatActivity {
@@ -24,5 +37,56 @@ public class MainMapActivity extends AppCompatActivity {
 
         tMapView.setSKTMapApiKey("5594960f-bb8d-46ea-94db-e4a68576b536");
         linearLayoutTmap.addView(tMapView);
+
+        TMapMarkerItem markerItem1 = new TMapMarkerItem();
+
+        TMapPoint tMapPoint1 = new TMapPoint(37.496263, 126.959167); //숭실대학교 창신관
+
+        //마커 아이콘
+        Bitmap bitmap = getBitmapFromVectorDrawable(getApplicationContext(), R.drawable.ic_map_pin);
+
+        markerItem1.setIcon(bitmap);
+        markerItem1.setPosition(0.5f, 1.0f);
+        markerItem1.setTMapPoint(tMapPoint1);
+        markerItem1.setName("숭실대학교 창신관");
+        tMapView.addMarkerItem("markerItem1", markerItem1);
+
+        tMapView.setCenterPoint(37.496263, 126.959167);
+
+        //BottomNavigation 구현
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(mainIntent);
+                        return true;
+                    case R.id.action_ar:
+                        return true;
+                    case R.id.action_search:
+                        return true;
+                    case R.id.action_map:
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    //벡터이미지에서 비트맵 변환
+    public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
+        Drawable drawable = ContextCompat.getDrawable(context, drawableId);
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            drawable = (DrawableCompat.wrap(drawable)).mutate();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
     }
 }
