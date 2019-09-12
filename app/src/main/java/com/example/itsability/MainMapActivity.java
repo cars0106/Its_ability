be988f56f2671163299f4bec999427bf37c8a5c9
cars0106 <cars0106@naver.com>
2019-09-12 18:02:45 +0900

    T-map Marker 변경
    Signed-off-by: cars0106 <cars0106@naver.com>

--- app/src/main/java/com/example/itsability/MainMapActivity.java
+++ app/src/main/java/com/example/itsability/MainMapActivity.java
@@ -8,23 +8,31 @@ import androidx.core.graphics.drawable.DrawableCompat;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.PointF;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 
+import java.util.ArrayList;
+
 public class MainMapActivity extends AppCompatActivity {
 
     /*
     https://stackoverflow.com/questions/33696488/getting-bitmap-from-vector-drawable
     Vector Drawable에서 Bitmap 객체 받는 방법
+
+    http://tmapapi.sktelecom.com/main.html#android/docs/androidDoc.TMapView_OnClickListenerCallback
+    T-map api 가이드
      */
 
     @Override
@@ -40,18 +48,17 @@ public class MainMapActivity extends AppCompatActivity {
 
         TMapMarkerItem markerItem1 = new TMapMarkerItem();
 
-        TMapPoint tMapPoint1 = new TMapPoint(37.496263, 126.959167); //숭실대학교 창신관
+        TMapPoint tMapPoint1 = new TMapPoint(37.496263, 126.959167); // 숭실대학교 창신관
 
-        //마커 아이콘
+        // 마커 아이콘
         Bitmap bitmap = getBitmapFromVectorDrawable(getApplicationContext(), R.drawable.ic_map_pin);
 
-        markerItem1.setIcon(bitmap);
-        markerItem1.setPosition(0.5f, 1.0f);
-        markerItem1.setTMapPoint(tMapPoint1);
-        markerItem1.setName("숭실대학교 창신관");
-        tMapView.addMarkerItem("markerItem1", markerItem1);
-
-        tMapView.setCenterPoint(37.496263, 126.959167);
+        markerItem1.setIcon(bitmap); // 마커 아이콘 지정
+        markerItem1.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
+        markerItem1.setTMapPoint( tMapPoint1 ); // 마커의 좌표 지정
+        markerItem1.setName("숭실대학교 창신관"); // 마커의 타이틀 지정
+        markerItem1.setID("marker1");
+        tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
 
         //BottomNavigation 구현
         BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
@@ -78,9 +85,8 @@ public class MainMapActivity extends AppCompatActivity {
     //벡터이미지에서 비트맵 변환
     public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
         Drawable drawable = ContextCompat.getDrawable(context, drawableId);
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            drawable = (DrawableCompat.wrap(drawable)).mutate();
-        }
+
+        drawable = (DrawableCompat.wrap(drawable)).mutate();
 
         Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
