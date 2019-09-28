7f457fc953cdad8ff6221ae21e9a5d0f5b059e74
SebinLee <lsbjeju@gmail.com>
2019-09-29 03:18:04 +0900

    Add DataFromServer
    Add Function that make TourAPI Call URL from data

--- app/src/main/java/com/example/itsability/DataFromServer.java
+++ app/src/main/java/com/example/itsability/DataFromServer.java
@@ -9,6 +9,7 @@ public class DataFromServer {
 
     private static List<Map<String,Object>> placeData = new ArrayList<>();
     private static List<String> placeNameforSearchIndex = new ArrayList<>();
+    private static final String tourAPIKey = "c5DXs4GAE2qWO%2BmeVvcCmSQIIXtCL9izfIzCA2%2BGJFkxuA4%2BapH9EXOR4fvRS0s3RrYuzL3ug8ducJchXZn9AQ%3D%3D"
 
 
     public void addData(Map<String,Object> data) {
@@ -37,5 +38,17 @@ public class DataFromServer {
         return placeData.size();
     }
 
+    public String returnTourAPIUrl(String LocationName) {
+        int index = placeNameforSearchIndex.indexOf(LocationName);
+        String url = placeData.get(index).get("tourAPI").toString();
 
+        if(url.isEmpty() == false) {
+            url = url.replace("인증키",tourAPIKey);
+            url = url.replace("MobileOS=ETC","MobileOS=AND");
+            url = url.replace("MobileApp=TourAPI3.0_Guide","MobileApp=itsability");
+            url = url+"&_type=json";
+        }
+
+        return url;
+    }
 }
