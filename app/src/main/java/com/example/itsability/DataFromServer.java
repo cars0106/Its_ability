55990c95bad0416434c54e4fb6e3a0af89899c25
SebinLee <lsbjeju@gmail.com>
2019-09-28 21:20:08 +0900

    Edit DataFromServer
    Add Function that get data from location name

--- app/src/main/java/com/example/itsability/DataFromServer.java
+++ app/src/main/java/com/example/itsability/DataFromServer.java
@@ -1,26 +1,24 @@
 package com.example.itsability;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DataFromServer {
 
     private static List<Map<String,Object>> placeData = new ArrayList<>();
+    private static List<String> placeNameforSearchIndex = new ArrayList<>();
+
 
     public void addData(Map<String,Object> data) {
         placeData.add(data);
+        placeNameforSearchIndex.add(data.get("Location").toString());
+    }
+
+    public Map<String,Object> getDataFromLocationName(String name) {
+        int index = placeNameforSearchIndex.indexOf(name);
+        return placeData.get(index);
     }
 
     public List<Map<String,Object>> returnData() {
